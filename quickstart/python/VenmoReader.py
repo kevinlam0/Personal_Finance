c221685fd0538c70ea430e81ad522689f60b0ad0
kevinlam0 <kevinclam02@gmail.com>
2024-12-20 14:40:10 -0500

    Creating table defintion for VenmoReader

---
+++ quickstart/python/VenmoReader.py
@@ -0,0 +1,37 @@
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+# Database configs
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+
+TABLE_CREATION_SQL = """CREATE TABLE IF NOT EXISTS VenmoTransaction(
+                        id SERIAL PRIMARY KEY,
+                        from VARCHAR(30) NOT NULL,
+                        to VARCHAR(30) NOT NULL,
+                        note VARCHAR(80) NOT NULL,
+                        amount DECIMAL(12,2) NOT NULL,
+                        date TIMESTAMP NOT NULL,
+                        ))"""
+
+
+class VenmoReader():
+
+    def connect(self):
+        return psycopg2.connect(
+            database = DATABASE_NAME, 
+            user = DATABASE_USER, 
+            host = DATABASE_HOST, 
+            password = DATABASE_PASSWORD, 
+            port = DATABASE_PORT)
+        
+    def create_tables(self):
+        conn = self.connect()
+        cur = conn.cursor()
+        
+        
